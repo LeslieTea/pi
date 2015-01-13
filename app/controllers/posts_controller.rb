2ca5f754607d3dbeb09d29006e191df0a7993800
LeslieTea <a.leslieann@gmail.com>
2015-01-13 03:45:59 +0000

    Various form types and navigation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,34 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+  
+  def show
+    @posts = Post.find(params[:id])
+  end
+  
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    
+    if @post.save
+      flash[:notice] = "Your post was created"
+      redirect_to posts_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+  
+  private
+  def post_params
+    params.require(:post).permit!
+  end
 end
