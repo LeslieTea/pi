f897cc86e777c427c88ba23a2485ae276be650b3
LeslieTea <a.leslieann@gmail.com>
2015-01-19 23:38:25 +0000

    Category Tab & Form

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ PostitTemplate::Application.routes.draw do
   root to: 'posts#index'
   
   resources :posts, except: [:destroy]
+  resources :categories, only: [:new, :create, :show]
   resources :comments, only: [:show, :edit, :update]
 end
